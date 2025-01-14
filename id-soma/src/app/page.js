d1b3f8f49ce8a43fcb103f896ff93620ce604aad
jhulialeal <lealjhu@gmail.com>
2025-01-14 10:35:33 -0300

    Restaurando alterações do stash

--- id-soma/src/app/page.js
+++ id-soma/src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Index from "./pages/Index";
 import CollaboratorsPage from "./pages/colaboradores";
+import LoginAdmin from "./pages/Login";
 
 export default function Home() {
   return (
