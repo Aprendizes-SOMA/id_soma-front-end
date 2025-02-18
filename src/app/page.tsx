5d2def94e2962d37b7ef387e803f0c45eb223e2a
arturmartins <arturmartinssilva078@gmail.com>
2025-02-18 10:23:16 -0300

    refactor(all): Movendo arquivos para raiz do projeto.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import styles from "../styles/page.module.css";
-import { listCollaboratorsByCPF } from "../app/api/collaborator/index";
+import { listCollaboratorsByCPF } from "./api/collaborator/index";
 import { useCollaborators } from "@/hooks/useCollaborators";
 
 
