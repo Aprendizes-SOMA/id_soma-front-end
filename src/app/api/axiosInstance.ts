b8d854178ce8dafc0ec4825468a244e94d6cb459
arturmartins <arturmartinssilva078@gmail.com>
2025-02-20 11:50:59 -0300

    refactor: atualizar configuração do servidor e ajustar scripts de inicialização

--- src/app/api/axiosInstance.ts
+++ src/app/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://id-soma.onrender.com",
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
